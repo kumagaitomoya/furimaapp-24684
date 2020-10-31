69fe73767e074258c78792d18d580884afe5c7ca
kumagaitomoya <72239344+kumagaitomoya@users.noreply.github.com>
2020-10-31 12:53:01 +0900

    エラーメッセージ 完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,25 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+    validates :password, presence: true,format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Password Include both letters and numbers' }
+    validates :nickname,:email,:birthday, presence: true
+    validates :email, uniqueness: true
 
-  validates :nickname,:first_name,:last_name,:first_name_kana,:last_name_kana,:birthday, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, presence: true, length: { minimum: 7 }, format: { with: /(?=.*\d+.*)(?=.*[a-zA-Z]+.*)./ }
+    validates :last_name,:first_name,:last_name_kana,:first_name_kana,
+               presence: true, presence: {message: 'can’t be black'}
+  
+     validates :first_name,:last_name,
+                format: {with:/\A[ぁ-んァ-ン一-龥]/,message: 'Full-width characters'}
+    validates  :first_name_kana,:last_name_kana,
+                format: {with: /\A[ァ-ヶー－]+\z/,message: 'Full-width katakana characters'}
+ 
 
 end
+
+
+
+
+
 
 
 
