9b81043bb4a2ef7b1d62391b1423100e3b26fdab
kumagaitomoya <72239344+kumagaitomoya@users.noreply.github.com>
2020-11-02 20:33:23 +0900

    ロボコップ使用完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,26 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    validates :password, presence: true,format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Password Include both letters and numbers' }
-    validates :nickname,:email,:birthday, presence: true
-    validates :email, uniqueness: true
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Password Include both letters and numbers' }
+  validates :nickname, :email, :birthday, presence: true
+  validates :email, uniqueness: true
 
-    validates :last_name,:first_name,:last_name_kana,:first_name_kana,
-               presence: true, presence: {message: 'can’t be black'}
-  
-     validates :first_name,:last_name,
-                format: {with:/\A[ぁ-んァ-ン一-龥]/,message: 'Full-width characters'}
-    validates  :first_name_kana,:last_name_kana,
-                format: {with: /\A[ァ-ヶー－]+\z/,message: 'Full-width katakana characters'}
- 
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana,
+            presence: true, presence: { message: 'can’t be black' }
 
+  validates :first_name, :last_name,
+            format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+  validates :first_name_kana, :last_name_kana,
+            format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
 end
-
-
-
-
-
-
-
-
-
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.name}
-    email                 {Faker::Internet.email}
-    password              {"Ab0508"}
-    password_confirmation {"Ab0508"}
-    last_name             {"山田"}
-    first_name            {"友弥"}
-    last_name_kana        {"ヤマダ"}
-    first_name_kana       {"タロウ"}
-    birthday              {"1993-05-08"}
+    nickname              { Faker::Name.name }
+    email                 { Faker::Internet.email }
+    password              { 'Ab0508' }
+    password_confirmation { 'Ab0508' }
+    last_name             { '山田' }
+    first_name            { '友弥' }
+    last_name_kana        { 'ヤマダ' }
+    first_name_kana       { 'タロウ' }
+    birthday              { '1993-05-08' }
   end
 end
