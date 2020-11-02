d3dd5a450e338b0d8b610cd892bccc034db8c3f7
kumagaitomoya <72239344+kumagaitomoya@users.noreply.github.com>
2020-11-02 20:21:05 +0900

    テストコード完了

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.name}
-    # email                 {"kkk@gmail.com"}
-    # password              {"00000000"}
-    # password_confirmation {password}
-    # last_name
-    # first_name
-    # last_name_kana
-    # first_name_kana
-    # birthday
+    email                 {Faker::Internet.email}
+    password              {"Ab0508"}
+    password_confirmation {"Ab0508"}
+    last_name             {"山田"}
+    first_name            {"友弥"}
+    last_name_kana        {"ヤマダ"}
+    first_name_kana       {"タロウ"}
+    birthday              {"1993-05-08"}
   end
 end
