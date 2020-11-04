94a2fbfeda95524a938e65ecdbb4131c967d762a
kumagaitomoya <72239344+kumagaitomoya@users.noreply.github.com>
2020-11-04 12:23:24 +0900

    バリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,21 @@ class Product < ApplicationRecord
   belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-
+  
+  validates :image, :name,:status,:price presence: true
   validates :category_id, numericality: { other_than: 1 }
 end
+
+
+
+# Image can't be blank 完了
+# Name can't be blank　完了
+# Info can't be blank　完了
+# Price can't be blank　完了
+# Price Half-width number 
+# Price Out of setting range
+# Category Select
+# Sales status Select
+# Shipping fee status Select
+# Prefecture Select
+# Scheduled delivery Select
