8b175f214069554a207ad014b748fff91e81c3c0
kumagaitomoya <72239344+kumagaitomoya@users.noreply.github.com>
2020-11-06 14:28:48 +0900

    ロボコップ完了

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,13 +11,13 @@ class Product < ApplicationRecord
   validates :image, :name, :status, presence: true
 
   with_options numericality: { other_than: 1, message: 'Select' } do
-  validates :category_id
-  validates :condition_id
-  validates :shipping_charges_id
-  validates :shipping_area_id
-  validates :shipping_days_id
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charges_id
+    validates :shipping_area_id
+    validates :shipping_days_id
   end
-  
+
   validates :price, presence: true, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
 end
