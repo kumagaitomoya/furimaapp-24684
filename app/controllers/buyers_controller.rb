f86de25a890aecedc95412dabcb0d3a4c370579a
kumagaitomoya <72239344+kumagaitomoya@users.noreply.github.com>
2020-11-10 11:36:13 +0900

    購入機能決済完了

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -8,20 +8,29 @@ class BuyersController < ApplicationController
   end
 
   def create
+    @product = Product.find(params[:product_id])
     @user_product = UserProduct.new (product_params)
-    # if  @user_product.valid?
-    #   @user_product.save
-    #   redirect_to action: :index
-    # else
-    #   render action: :new
-    # end
+    if  @user_product.valid?
+      # binding.pry
+      pay_product
+       @user_product.save
+       redirect_to action: :index
+     end
   end
 
   private 
 
   def product_params
-  params.require(:user_product).permit(:postal_code, :municipality, :address, :phone_namber, :buiding_name,:shipping_area_id,).merge(user_id: current_user.id,product_id: params[:product_id])
+  params.require(:user_product).permit(:postal_code, :municipality, :address, :phone_namber, :buiding_name,:shipping_area_id,).merge(user_id: current_user.id,product_id: params[:product_id],token: params[:token])
   end
 
+  def pay_product
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @product.price,  
+      card: product_params[:token],    
+      currency: 'jpy'                
+    )
+  end
 end
 
