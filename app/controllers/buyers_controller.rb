e3d1c5ef69fb4481ea657fd4a4b57db7a3a3540a
kumagaitomoya <72239344+kumagaitomoya@users.noreply.github.com>
2020-11-11 11:53:45 +0900

    コントローラー修正

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -4,6 +4,7 @@ class BuyersController < ApplicationController
   
   def index
     @user_product = UserProduct.new
+
      if current_user.id == @product.user_id || @product.buyer.present?
        redirect_to root_path
      else
@@ -15,7 +16,6 @@ class BuyersController < ApplicationController
   end
 
   def create
-    @product = Product.find(params[:product_id])
     @user_product = UserProduct.new(product_params)
     if @user_product.valid?
       pay_product
@@ -43,6 +43,6 @@ class BuyersController < ApplicationController
 end
 
 def set_product
-  @product = Product.find(params[:id])
+  @product = Product.find(params[:product_id])
 end
 
